06efdb53f8f5e5145d74a6093d98cabe6e40044a
Zayan <zayan_ahmad@hotmail.com>
2024-07-31 12:21:50 -0500

    Test JTree with File class

--- src/Frame.java
+++ src/Frame.java
@@ -1,21 +1,37 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+// import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
+import java.io.File;
 
 public class Frame extends JFrame {
 
     public Frame() {
-        frameConfig();
+
+        JTree tree = new JTree(File.listRoots());
+
+        tree.setFocusable(false);
+
+        this.add(new JScrollPane(tree));
+
+        JLabel label = new JLabel();
+        this.add(label, BorderLayout.SOUTH);
+
+        tree.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                label.setText("Selection is: " + e.getPath());
+            }
+        });
+
+        this.configure();
     }
 
-    public void frameConfig() {
-        this.setSize(Frame.getScreenSize());
-        this.setLayout(null);
-        this.setVisible(true);
+    private void configure() {
+        this.setSize(500, 625);
+        this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
-
-    public static Dimension getScreenSize() {
-        return Toolkit.getDefaultToolkit().getScreenSize();
-    }
-
 }
