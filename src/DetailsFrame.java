2604199c09c3a1bbe3b8ad732aa6d2b568dcd102
Zayan <zayan_ahmad@hotmail.com>
2024-08-02 14:30:28 -0500

    Added DetailsFrame

---
+++ src/DetailsFrame.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.io.File;
+import java.awt.Dimension;
+
+public class DetailsFrame extends JFrame {
+
+    private final FileNode node;
+    private final JTextArea infoArea;
+
+    public DetailsFrame(FileNode node) {
+
+        super("Details for:" + node.getFile().getName());
+
+        this.node = node;
+
+        infoArea = new JTextArea();
+        infoArea.setEditable(false);
+        infoArea.setText(getDetails());
+
+        this.add(new JScrollPane(this.infoArea));
+
+        this.configure();
+    }
+
+    public String getDetails() {
+        StringBuilder sb = new StringBuilder();
+
+        File file = node.getFile();
+
+        sb.append("Name: ").append(file.getName()).append("\n");
+        sb.append("Path: ").append(file.getPath()).append("\n");
+        sb.append("Size: ").append(file.length()).append(" bytes").append("\n");
+        sb.append("Readable: ").append(file.canRead()).append("\n");
+        sb.append("Writable: ").append(file.canWrite()).append("\n");
+        sb.append("Executable: ").append(file.canExecute()).append("\n");
+        sb.append("Is Directory: ").append(file.isDirectory()).append("\n");
+        sb.append("Last Modified: ").append(file.lastModified()).append("\n");
+        return sb.toString();
+
+    }
+
+    private void configure() {
+        this.setSize(300, 400);
+
+        this.setMinimumSize(new Dimension(300, 450));
+        this.setMaximumSize(new Dimension(400, 550));
+
+        this.setResizable(true);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+}
